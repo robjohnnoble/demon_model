876a6666a4f1147a9c4cd0c262f389929f545683
vesmanojlovic <manojlovicveselin@gmail.com>
2024-02-26 16:14:54 +0000

    typo in output file header

--- src/output.cpp
+++ src/output.cpp
@@ -1,11 +1,11 @@
 #include "output.hpp"
 
 void FileOutput::writeDemesHeader() {
-    file << "Generation, Deme, Side, Population, OriginTime, AverageArray" << std::endl;
+    file << "Generation,Deme,Side,Population,OriginTime,AverageArray" << std::endl;
 }
 void FileOutput::writeDemesFile(Tumour& tumour) {
     for (int i = 0; i < tumour.getNumDemes(); i++) {
-        file << tumour.getGensElapsed() << ", " << i << ", " << tumour.getDeme(i).getSide() << ", " << tumour.getDeme(i).getPopulation() << ", " << tumour.getDeme(i).getOriginTime() << ", ";
+        file << tumour.getGensElapsed() << "," << i << "," << tumour.getDeme(i).getSide() << "," << tumour.getDeme(i).getPopulation() << "," << tumour.getDeme(i).getOriginTime() << ",";
         for (int j = 0; j < tumour.getDeme(i).getAverageArray().size(); j++) {
             file << tumour.getDeme(i).getAverageArray()[j] << " ";
         }
